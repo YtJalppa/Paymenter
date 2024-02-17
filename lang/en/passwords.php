f5902170872e75f5cebcc2ac1f30f5de41b9baba
Jalppa <139807986+YtJalppa@users.noreply.github.com>
2024-02-17 22:15:05 +0200

    Update passwords.php

--- lang/en/passwords.php
+++ lang/en/passwords.php
@@ -12,9 +12,9 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Salasanasi on nollattu!',
+    'sent' => 'Lähetimme sinulle sähköpostiin salasanan nollauslinkin!',
+    'throttled' => 'Odota ennen kuin teet uudelleen.',
+    'token' => 'Salasanan nollaustokeni on väärä.',
+    'user' => "Käyttäjää tuolla sähköpostiosoitteella ei ole.",
 ];
