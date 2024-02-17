ebd06b885adf7dbd08356fad60ad2c51128e3f28
Jalppa <139807986+YtJalppa@users.noreply.github.com>
2024-02-17 22:13:30 +0200

    Update auth.php

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -12,7 +12,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Tiedot ovat väärin.',
+    'password' => 'Annettu salasana on väärin.',
+    'throttle' => 'Liian monta epäonnistunutta yritystä. Yritä uudelleen :seconds sekuntin kuluttua.',
 ];
